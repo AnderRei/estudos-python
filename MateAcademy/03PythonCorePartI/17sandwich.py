b9ad0fde702294a69d91685cfb8775d2852d5baa
Anderson Reichardt <andersonia@yahoo.com.br>
2026-03-23 21:58:55 -0300

    new lessons added

---
+++ MateAcademy/03PythonCorePartI/17sandwich.py
@@ -0,0 +1,34 @@
+# Decorators
+
+
+from functools import wraps
+from typing import Callable, Any
+
+
+def bread(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        print("</--------\\>")
+        func(*args, **kwargs)
+        print("<\\________/>")
+
+    return wrapper
+
+
+def ingredients(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        print(" #tomatoes#")
+        func(*args, **kwargs)
+        print("   ~salad~")
+
+    return wrapper
+
+
+@bread
+@ingredients
+def sandwich()-> None:
+    print("   --ham--")
+
+
+sandwich()
